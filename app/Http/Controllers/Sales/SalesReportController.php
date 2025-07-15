d131c7b150be777768cbce4b44964763a83ef5ab
jhondelee <jhondelee@gmail.com>
2025-07-15 13:07:45 +0800

    Update SalesReportController.php

--- app/Http/Controllers/Sales/SalesReportController.php
+++ app/Http/Controllers/Sales/SalesReportController.php
@@ -116,7 +116,7 @@ class SalesReportController extends Controller
             $pdf::Ln(5);
             $pdf::SetFont('Arial','',9);
             $pdf::cell(20,6,$payment->date_payment,0,"","L");
-            $pdf::cell(30,6,$payment->address,0,"","L");
+            $pdf::cell(35,6,$payment->address,0,"","L");
             $pdf::cell(25,6,$payment->so_number,0,"","L");
             $pdf::cell(35,6,$payment->cs_name,0,"","L");
             $pdf::cell(30,6,$payment->paymode,0,"","L");
