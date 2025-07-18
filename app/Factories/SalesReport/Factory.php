d86cdb9f94d6c503275548b467e17d9c815f6b46
jhondelee <jhondelee@gmail.com>
2025-07-18 11:47:45 +0800

    Update Factory.php

--- app/Factories/SalesReport/Factory.php
+++ app/Factories/SalesReport/Factory.php
@@ -20,7 +20,7 @@ class Factory implements SetInterface
     {
         $results = DB::select("SELECT DATE_FORMAT(e.date_payment,'%m-%d-%Y') as date_payment, c.address, o.so_number, c.name AS cs_name , m.name AS paymode, e.amount_collected,s.payment_status
             FROM sales_payment_terms e
-            INNER JOIN sales_payment s ON s.id = e.sales_payment_id
+            LEFT JOIN sales_payment s ON s.id = e.sales_payment_id
             INNER JOIN sales_order o ON o.id = s.sales_order_id
             INNER JOIN customers c ON c.id = o.customer_id
             INNER JOIN mode_of_payments m ON m.id = e.payment_mode_id
