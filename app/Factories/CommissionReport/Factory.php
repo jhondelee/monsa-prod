92e5078c0850209091708dadfb645056404baa32
jhondelee <jhondelee@gmail.com>
2025-08-07 16:37:54 +0800

    Update Factory.php

--- app/Factories/CommissionReport/Factory.php
+++ app/Factories/CommissionReport/Factory.php
@@ -19,7 +19,7 @@ class Factory implements SetInterface
         INNER JOIN customers c ON c.id = so.customer_id
         INNER JOIN areas a ON a.id = c.area_id
         WHERE so.so_date BETWEEN ? AND ? AND so.employee_id = ?
-        GROUP BY so.so_date, a.name
+        GROUP BY  a.name,so.so_date
         ORDER BY  a.name,so.so_date",[$start, $end, $empID]);
 
         return collect($results);
@@ -31,9 +31,9 @@ class Factory implements SetInterface
         SELECT 
             DATE_FORMAT(so.so_date,'%m-%d-%Y')  AS so_date , 
             a.name AS area, 
-            ( sp.sales_total + ifnull(sum(ri.srp),0)) AS total_sales,
-            sum(ri.srp) AS total_returns,
-            ( sp.sales_total + ifnull(sum(ri.srp),0)) - ifnull(sum(ri.srp),0) AS total_amount,
+            (sp.sales_total + IFNULL(r.amount,0)) AS total_sales,
+            r.amount AS total_returns,
+            so.total_sales AS total_amount,
             (cr.rate) AS rates
         FROM sales_payment sp
         INNER JOIN sales_order so ON so.id = sp.sales_order_id AND so.status='CLOSED'
@@ -44,7 +44,7 @@ class Factory implements SetInterface
         LEFT  JOIN returns r ON r.so_number = sp.so_number
         LEFT JOIN return_items ri ON r.id = ri.returns_id
         WHERE so.so_date BETWEEN ? AND ? AND so.employee_id = ?
-        GROUP BY a.name,so.so_date, sp.sales_total,cr.rate
+        GROUP BY a.name,so.so_date,sp.sales_total,so.total_sales,cr.rate,r.amount
         ORDER BY  a.name,so.so_date",[$start, $end, $empID]);
 
         return collect($results);
