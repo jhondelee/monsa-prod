af22cd38e6a720f302703da75f20319d02c0699b
jhondelee <jhondelee@gmail.com>
2025-09-09 10:45:07 +0800

    Update SalesController.php

--- app/Http/Controllers/Sales/SalesController.php
+++ app/Http/Controllers/Sales/SalesController.php
@@ -625,8 +625,7 @@ class SalesController extends Controller
                         $pdf::SetFont('Arial','',11);
                         $pdf::cell(30,6,"_________________________________________________________________________________________________________",0,"","L");
                                $pdf::Ln(5);
-                            $pdf::SetFont('Arial','I',11);
-
+                            $pdf::SetFont('Arial','I',9);
                             $pdf::cell(185,6,"--Nothing Follows--",0,"","C");
 
                         $pdf::Ln(3);
