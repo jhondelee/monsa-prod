f033f4d49d9bf15fd40bb2c602e5f885fb519b8f
jhondelee <jhondelee@gmail.com>
2025-10-13 12:32:01 +0800

    Update index.blade.php

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -15,7 +15,7 @@
                     <div class="ibox-content">
                         <h1 class="no-margins">{{number_format($sales_total)}}</h1>
                         <div class="stat-percent font-bold text-success">{{number_format($sales_percent)}}% <i class="fa fa-bolt"></i></div>
-                        <small>Total Salesssssssss</small>
+                        <small>Total Sales</small>
                     </div>
                 </div>
             </div>
